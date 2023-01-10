b0907f2524119d983b1ec73f3786654b2223d14f
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-10 07:12:29 +0100

    initial commit, header component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import airbnbLogo from '../assets/airbnb.png';
+
+export default function () {
+  return (
+    <header>
+      <img src={airbnbLogo} className="logo" alt="airbnb" />
+    </header>
+  );
+}
